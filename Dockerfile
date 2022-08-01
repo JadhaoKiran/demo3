42ab7fea8ee9e7e8e5444c8847bfe5307327cdb9
JadhaoKiran <jadhaokgdhanu@gmail.com>
2022-08-01 13:54:30 +0530

    demo with docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:7
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+COPY ["package.json", "package-lock.json*", "./"]
+
+RUN npm install --production
+
+COPY . .
+
+CMD [ "node", "server.js" ]
+EXPOSE 7000
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+
+app.get("/", (req, resp)=>
+{
+    " <h3>Welcome to exam </h3> "
+    + "<h4>List of Names </h4>"
+    + "<ol>"
+    +"<li> Kiran</li>"
+    +"<li> Kiran</li>"
+    +"<li> Kiran</li>"
+    +"</ol>"
+});
+
+app.get("/home", (req,resp)=>{
+
+    "<h1>Welcome to Home Page </h1>"
+})
+
+app.listen(7000);
+console.log("server start at port 7000");
